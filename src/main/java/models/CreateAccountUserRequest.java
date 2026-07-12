b448c974172556635bc6a2662276847913f394f8
N.Sysoev <sysoevnick@gmail.com>
2026-07-12 18:02:44 +0300

    удалены лишние зависимости Сделаны фиксы по методу депозита

--- src/main/java/models/CreateAccountUserRequest.java
+++ src/main/java/models/CreateAccountUserRequest.java
@@ -1,13 +1,8 @@
 package models;
 
-import io.restassured.response.ValidatableResponse;
-import io.restassured.specification.RequestSpecification;
-import io.restassured.specification.ResponseSpecification;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import requests.Request;
 
 @Data
 @AllArgsConstructor
--- src/main/java/models/DepositMoneyUserResponse.java
+++ src/main/java/models/DepositMoneyUserResponse.java
@@ -1,13 +1,11 @@
 package models;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
-import java.util.Date;
 import java.util.List;
 
 @Data
